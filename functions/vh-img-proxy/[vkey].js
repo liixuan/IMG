f2c41af3ab0342c7b241bc7a00afbb67bd51d29f
李宜轩 <109458894+liixuan@users.noreply.github.com>
2025-03-14 23:22:22 +0800

    Update [vkey].js

--- functions/vh-img-proxy/[vkey].js
+++ functions/vh-img-proxy/[vkey].js
@@ -5,7 +5,7 @@ export async function onRequestGet({ request }) {
     method,
     headers: {
       ...headers,
-      referer: 'https://www.vhimg.com',
+      referer: 'https://www.eexuan.cn',
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
     },
     body
